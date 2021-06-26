cca8ac1e95eea7b0d10f7c37cdc2b184c021e68e
Camille-Louise <brunnodottir@gmail.com>
2021-06-26 12:31:06 +0200

    add form entry tests 1)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,21 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const btnClose = document.getElementsByClassName('close')[0]
+const btnSubmit = document.getElementsByClassName('btn-submit');
+//Form DOM Elements
 const firstNameInput = document.getElementById("first");
+const lastNameInput = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdateInput = document.getElementById("birthdate");
+const tournoi = document.getElementById("quantity");
+const newYork = document.getElementById("location1");
+const sanFrancisco = document.getElementById("location2");
+const seattle = document.getElementById("location3");
+const chicago = document.getElementById("location4");
+const boston = document.getElementById("location5");
+const portland = document.getElementById("location6");
+const villes = [newYork, sanFrancisco, seattle, chicago, boston, portland];
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -22,25 +36,86 @@ function launchModal() {
 }
 
 
-// close modal form
+// close modal form on click
 function closeModal() {
   modalbg.style.display ="none";
 }
 
-const btnClose = document.getElementsByClassName('close')[0]
-console.log(btnClose);
 
 btnClose
 .addEventListener("click", closeModal
 
+
 );
 
-// form
-
-const form = document.forms[0]
-console.log(form);
-
+// submit form
 form.addEventListener("submit", function(e){
- e.preventDefault();
-  console.log(firstNameInput.value)
+  e.preventDefault();
+
+
+// form validation
+
+// NAME
+
+firstNameInput.addEventListener('input', function(e) {
+  var value = e.target.value;
+  if (value.startsWith('Hello ')) {
+      isValid = true;
+  } else {
+      isValid = false;
+  }
 });
+
+// PRENOM
+
+lastNameInput.addEventListener('input', function(e) {
+  var value = e.target.value;
+  if (value.startsWith('Hello')){
+      isValid = true;
+      console.log('Hello');
+  } else {
+    isValid =false;
+    console.log('Oups');
+  }
+});
+
+
+//const firstNameInput if (firstNameInput.value == valeur) {console.log ('test')}
+
+//EMAIL
+
+email.addEventListener('input', function(e) {
+  var value = e.target.value;
+  if (value.startsWith('Hello')){
+      isValid = true;
+      console.log('Hello');
+  } else {
+    isValid =false;
+    console.log('Oups');
+  }
+});
+
+//BIRTHDATE
+
+birthdateInput.addEventListener('input', function(e) {
+  var value = e.target.value;
+  if (value.startsWith('Hello')){
+      isValid = true;
+      console.log('Hello');
+  } else {
+    isValid =false;
+    console.log('Oups');
+  }
+});
+//NUMBER
+
+///CITY
+
+
+const formValid = document.getElementById("submit");
+  
+
+
+})
+
+
