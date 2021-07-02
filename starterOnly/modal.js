5222f83353a6d532717188586df9cffd6ea7d528
Camille-Louise <brunnodottir@gmail.com>
2021-07-02 12:59:30 +0200

    add validation functions for form entries + correct css and html + start submitted form function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,13 +19,122 @@ const lastNameInput = document.getElementById("last");
 const email = document.getElementById("email");
 const birthdateInput = document.getElementById("birthdate");
 const tournoi = document.getElementById("quantity");
-const newYork = document.getElementById("location1");
-const sanFrancisco = document.getElementById("location2");
-const seattle = document.getElementById("location3");
-const chicago = document.getElementById("location4");
-const boston = document.getElementById("location5");
-const portland = document.getElementById("location6");
-const villes = [newYork, sanFrancisco, seattle, chicago, boston, portland];
+const city = document.querySelectorAll ("#city .checkbox-input")
+const cityLocation = document.getElementById("city")
+const checkbox1 = document.getElementById("checkbox1")
+
+// NAME
+const isFirstValid = () => {
+  const value = firstNameInput.value;
+  if (value.trim().length >= 2) {
+    formData[0].setAttribute("data-error-visible", "false")
+      return true;
+  } else {
+    formData[0].setAttribute("data-error", "Veuillez renseigner votre prénom")
+    formData[0].setAttribute("data-error-visible", "true")
+
+      return false;
+
+  }
+}
+
+const isLastValid = () => {
+  const value = lastNameInput.value;
+  if (value.trim().length >= 2 ) {
+    formData[1].setAttribute("data-error-visible", "false")
+    return true;
+  } else {
+    formData[1].setAttribute("data-error", "Veuillez renseigner votre nom")
+    formData[1].setAttribute("data-error-visible", "true")
+
+  return false;
+
+  }
+}
+
+//EMAIL 
+const isEmailValid = () => {
+  const value = email.value;
+  const atPosition = value.indexOf("@");
+  const dotPosition = value.lastIndexOf(".");
+  if (atPosition>1 || dotPosition>atPosition+2 || dotPosition+2<=value.length){
+    formData[2].setAttribute("data-error-visible", "false")
+    return true;
+  }else{
+    formData[2].setAttribute("data-error", "Veuillez renseigner une adresse email valide")
+    formData[2].setAttribute("data-error-visible", "true")
+
+    return false
+
+
+  }
+  
+}
+
+//DATE
+const isDateValid =() =>{
+
+  const value = birthdateInput.value;
+  const birthdate = new Date(value);
+  const now = new Date();
+  console.log(now - birthdate);
+  if (now - birthdate > 0) {
+    formData[3].setAttribute("data-error-visible", "false")
+      return true;
+  } else {
+    formData[3].setAttribute("data-error", "Veuillez renseigner une date")
+    formData[3].setAttribute("data-error-visible", "true")
+
+      return false;
+
+  }
+
+}
+
+// QUANTITY
+const isQuantityValid =() =>{
+
+  const value = tournoi.value;
+  if (value != "") {
+    formData[4].setAttribute("data-error-visible", "false")
+    return true;
+ }else{
+  formData[4].setAttribute("data-error", "Veuillez renseigner un nombre")
+  formData[4].setAttribute("data-error-visible", "true")
+   
+  return false;
+
+ } 
+}
+
+//location
+
+const isCityValid =() => {
+
+cityLocation.setAttribute("data-error-visible", "true")
+for (let i = 0; i< city.length; i++) {
+  if (city[i].checked){
+    cityLocation.setAttribute("data-error-visible", "false")
+    return true
+
+}else {
+  return false;
+}
+}}
+
+// Conditions d'utilisation géénrales
+const isCheckboxValid = () => {
+  if(checkbox1.checked === true){
+    formData[4].setAttribute("data-error-visible", "false")
+    return true;
+ }else{
+  formData[6].setAttribute("data-error", "Veuillez accepter les conditions d'utilisation")
+  formData[6].setAttribute("data-error-visible", "true")
+   
+  return false;
+  }
+}
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -48,74 +157,61 @@ btnClose
 
 );
 
-// submit form
+//  form VALIDATION
 form.addEventListener("submit", function(e){
   e.preventDefault();
+
+if (
+  isFirstValid() && isDateValid() && isLastValid() && isEmailValid && isQuantityValid && isCityValid && isCheckboxValid ) {
+console.log("Ok") // launch modalSubmittedForm + .reset
+//+ message (alert ? = formulaire validé !)
+  } else {
+    console.log('Erreur'); // faire une const check ?
+  }
 
 
 // form validation
 
-// NAME
 
-firstNameInput.addEventListener('input', function(e) {
-  var value = e.target.value;
-  if (value.startsWith('Hello ')) {
-      isValid = true;
-  } else {
-      isValid = false;
-  }
-});
 
-// PRENOM
+//console.log(isFirstValid());
+// VERIFICATION DES CHAMPS
+isFirstValid
+isLastValid();
+isDateValid();
+isEmailValid();
+isQuantityValid();
+isCityValid();
+isCheckboxValid();
 
-lastNameInput.addEventListener('input', function(e) {
-  var value = e.target.value;
-  if (value.startsWith('Hello')){
-      isValid = true;
-      console.log('Hello');
-  } else {
-    isValid =false;
-    console.log('Oups');
-  }
-});
+
+
 
 
 //const firstNameInput if (firstNameInput.value == valeur) {console.log ('test')}
 
-//EMAIL
-
-email.addEventListener('input', function(e) {
-  var value = e.target.value;
-  if (value.startsWith('Hello')){
-      isValid = true;
-      console.log('Hello');
-  } else {
-    isValid =false;
-    console.log('Oups');
-  }
-});
-
-//BIRTHDATE
-
-birthdateInput.addEventListener('input', function(e) {
-  var value = e.target.value;
-  if (value.startsWith('Hello')){
-      isValid = true;
-      console.log('Hello');
-  } else {
-    isValid =false;
-    console.log('Oups');
-  }
-});
-//NUMBER
-
-///CITY
 
 
-const formValid = document.getElementById("submit");
-  
 
+// VALIDATION TOUS
 
 })
+
+
+// const de validation finale =() => {
+// if (isFirstNameValid() === true && 
+// isLastNameValid( === true && etc.)
+// return true
+// else return false
+
+//SOUMISSION FORMULAIRE
+//* form.addEventListener("submit", (e) =>{ *bloque le raffraichissement de la page = e.preventefault});
+//if (const de validation finale === true )
+// lauch modal completed
+// else => affiche les champs mal remplis
+// checkAllFields
+
+
+
 
 
