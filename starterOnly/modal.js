64bb1c89d4a0ce084ee37b705a32bbb9d608d1e2
Camille-Louise <brunnodottir@gmail.com>
2021-07-22 14:14:36 +0200

    -final commit minor fixing in css

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,12 +19,12 @@ const lastNameInput = document.getElementById("last");
 const email = document.getElementById("email");
 const birthdateInput = document.getElementById("birthdate");
 const tournoi = document.getElementById("quantity");
-const city = document.querySelectorAll ("#city .checkbox-input");
+const city = document.querySelectorAll("#city .checkbox-input");
 const cityLocation = document.getElementById("city");
 const checkbox1 = document.getElementById("checkbox1")
 
 // Confirmed form DOM Element
-const confirmModal= document.querySelector(".confirm-modal-submit");
+const confirmModal = document.querySelector(".confirm-modal-submit");
 const btnSubmit = document.getElementsByClassName('btn-submit');
 const btnCloseSubmit = document.getElementsByClassName('close')[1]
 const btnConfirm = document.getElementById("btn-confirm");
@@ -36,13 +36,13 @@ const isFirstValid = () => {
   //Si le nom fait moins de 2 caractères
   if (value.trim().length >= 2) {
     formData[0].setAttribute("data-error-visible", "false")
-      return true;
+    return true;
   } else {
     //Une erreur est survenue
     formData[0].setAttribute("data-error", "Veuillez renseigner votre prénom")
     formData[0].setAttribute("data-error-visible", "true")
 
-      return false;
+    return false;
 
   }
 }
@@ -50,29 +50,29 @@ const isFirstValid = () => {
 // LASTNAME Validation
 const isLastValid = () => {
   const value = lastNameInput.value;
-    //Si le nom fait moins de 2 caractères
-  if (value.trim().length >= 2 ) {
+  //Si le nom fait moins de 2 caractères
+  if (value.trim().length >= 2) {
     formData[1].setAttribute("data-error-visible", "false")
     return true;
   } else {
-        //Une erreur est survenue
+    //Une erreur est survenue
     formData[1].setAttribute("data-error", "Veuillez renseigner votre nom")
     formData[1].setAttribute("data-error-visible", "true")
 
-  return false;
+    return false;
 
   }
 }
 
-//EMAIL 
+//EMAIL Validation
 const isEmailValid = () => {
   const value = email.value;
   const atPosition = value.indexOf("@");
   const dotPosition = value.lastIndexOf(".");
-  if (atPosition>1 || dotPosition>atPosition+2 || dotPosition+2<=value.length){
+  if (atPosition > 1 || dotPosition > atPosition + 2 || dotPosition + 2 <= value.length) {
     formData[2].setAttribute("data-error-visible", "false")
     return true;
-  }else{
+  } else {
     formData[2].setAttribute("data-error", "Veuillez renseigner une adresse email valide")
     formData[2].setAttribute("data-error-visible", "true")
 
@@ -80,79 +80,80 @@ const isEmailValid = () => {
 
 
   }
-  
+
 }
 
-//DATE
-const isDateValid =() =>{
+//BIRTHDATE VALIDATION
+const isDateValid = () => {
+  const formDataParent = birthdateInput.parentNode;
+  console.log(formDataParent)
 
   const value = birthdateInput.value;
   const birthdate = new Date(value);
   const now = new Date();
-  console.log(now - birthdate);
-  if (now - birthdate > 0) {
-    formData[3].setAttribute("data-error-visible", "false")
-      return true;
+  let minDate = new Date("1935-01-01");
+  
+  if (now - birthdate > 0  || birthdate < minDate) {
+    formDataParent.setAttribute("data-error-visible", "false")
+    return true;
   } else {
-    formData[3].setAttribute("data-error", "Veuillez renseigner une date")
-    formData[3].setAttribute("data-error-visible", "true")
+    formDataParent.setAttribute("data-error", "Veuillez renseigner une date")
+    formDataParent.setAttribute("data-error-visible", "true")
 
-      return false;
+    return false;
 
   }
 
 }
 
-// QUANTITY
-const isQuantityValid =() =>{
+// QUANTITY VALIDATION
+const isQuantityValid = () => {
   const value = tournoi.value;
-  console.log(formData[4]);
-  if (value !== "") {
+  let regexQuantity = /^[1-9]\d*$/gm;
+  if (value !== "" && regexQuantity.test(value)) {
     formData[4].setAttribute("data-error-visible", "false")
-    console.log("ok");
     return true;
- }else{
-  formData[4].setAttribute("data-error", "Veuillez renseigner un nombre")
-  formData[4].setAttribute("data-error-visible", "true")
-  return false;
+  } else {
+    formData[4].setAttribute("data-error", "Veuillez renseigner un nombre")
+    formData[4].setAttribute("data-error-visible", "true")
+    console.log("test")
+    return false;
 
- } 
+  }
 }
 
-//location
+//LOCATION VALIDATION
 
-const isCityValid =() => {
+const isCityValid = () => {
 
-let isChecked = false;
+  let isChecked = false;
 
-for (let i = 0; i< city.length; i++) {
-  if (city[i].checked){
-    isChecked = true;
+  for (let i = 0; i < city.length; i++) {
+    if (city[i].checked) {
+      isChecked = true;
+    }
+
+  }
+  if (!isChecked) {
+    formData[5].setAttribute("data-error", "Veuillez sélectionner une ville")
+    formData[5].setAttribute("data-error-visible", "true")
+    return false;
+  } else {
+    cityLocation.setAttribute("data-error-visible", "false")
+    return true
+  }
 }
 
-}
-console.log(isChecked)
-if (!isChecked)
-{
-  formData[5].setAttribute("data-error", "Veuillez sélectionner une ville")
-  formData[5].setAttribute("data-error-visible", "true")
- return false;
-} else {
-  cityLocation.setAttribute("data-error-visible", "false")
-return true
-}
-}
-
-// Conditions d'utilisation géénrales
+// CHECKBOX VALIDATION (Conditions d'utilisation générales)
 const isCheckboxValid = () => {
-  if(checkbox1.checked === true){
-    formData[4].setAttribute("data-error-visible", "false")
+  if (checkbox1.checked === true) {
+    formData[6].setAttribute("data-error-visible", "false")
     return true;
- }else{
-  formData[6].setAttribute("data-error", "Veuillez accepter les conditions d'utilisation")
-  formData[6].setAttribute("data-error-visible", "true")
-   
-  return false;
+  } else {
+    formData[6].setAttribute("data-error", "Veuillez accepter les conditions d'utilisation")
+    formData[6].setAttribute("data-error-visible", "true")
+
+    return false;
   }
 }
 
@@ -167,40 +168,40 @@ function launchModal() {
 
 // launch modal confirmed
 function launchValidationModal() {
-  confirmModal.style.display ="block";
+  confirmModal.style.display = "block";
 
 }
 
 // close modal form on click
 function closeModal() {
-  modalbg.style.display ="none";
+  modalbg.style.display = "none";
 }
 
 
 btnClose
-.addEventListener("click", closeModal
+  .addEventListener("click", closeModal
 
 
-);
+  );
 
 // close modal confirmed on click
 
-function closeValidationModal(){
-  confirmModal.style.display ="none";
+function closeValidationModal() {
+  confirmModal.style.display = "none";
 
 }
 
 btnCloseSubmit
-.addEventListener("click", closeValidationModal)
+  .addEventListener("click", closeValidationModal)
 btnConfirm
-.addEventListener("click", closeValidationModal)
+  .addEventListener("click", closeValidationModal)
 
 
 
 
-//  form VALIDATION
-form.addEventListener("submit", function(e){
-  e.preventDefault();
+// ALL FIELDS form VALIDATION (Vérification de tous les champs)
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); //Bloque le rafraichissement de la page//
 
   const validFirstName = isFirstValid();
   const validLastName = isLastValid();
@@ -209,15 +210,15 @@ form.addEventListener("submit", function(e){
   const validQuantity = isQuantityValid();
   const validCity = isCityValid();
   const validCheckbox = isCheckboxValid();
-if (
-  validFirstName && validLastName   && validBirthDate &&  validEmail && validQuantity && validCity && validCheckbox ) {
-launchModal()
-document.querySelector('form').reset();
-closeModal();
-launchValidationModal()
+  if (
+    validFirstName && validLastName && validBirthDate && validEmail && validQuantity && validCity && validCheckbox) {
+    launchModal()
+    document.querySelector('form').reset(); //Efface les champs après soumission//
+    closeModal();
+    launchValidationModal() // lance le message de confirmation//
 
   } else {
-    console.log('Erreur'); // faire une const check ?
+    return false
   }
 
 
@@ -225,14 +226,6 @@ launchValidationModal()
 
 
 
-
-//SOUMISSION FORMULAIRE
-
-//* form.addEventListener("submit", (e) =>{ *bloque le raffraichissement de la page = e.preventefault});
-//if (const de validation finale === true )
-// lauch modal completed
-// else => affiche les champs mal remplis
-// checkAllFields
 
 
 
